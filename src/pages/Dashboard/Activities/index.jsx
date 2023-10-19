0168a66f7f5055d82c97204ec691c9267811dbd3
Wesley da Silva Ribeiro <wesleysilvaribeiro9@gmail.com>
2023-10-19 16:06:41 -0300

    [FEAT] users that didn't pay their ticket cannot choose activities

--- src/pages/Dashboard/Activities/index.jsx
+++ src/pages/Dashboard/Activities/index.jsx
@@ -1,7 +1,35 @@
 import { useEffect } from 'react';
-
 import styled from 'styled-components';
+import useTicket from '../../../hooks/api/useTicket';
 
 export default function Activities() {
+  const { ticket } = useTicket();
+
+  useEffect(() => {
+  }, [ticket]);
+
+  if (ticket?.status !== 'PAID') {
+    return (
+      <ErrorMsgContainer>
+        Você precisa ter confirmado pagamento antes <br /> de fazer a escolha de atividades
+      </ErrorMsgContainer>
+    );
+  }
+
   return 'Atividades: Em breve!';
 }
+
+const ErrorMsgContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #8e8e8e;
+  text-align: center;
+  font-family: Roboto;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  height: 100%;
+  cursor: not-allowed;
+`;
