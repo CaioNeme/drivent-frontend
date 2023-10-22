564de0f556d6b324ed5a447e2eebfce7beb13041
Caio Neme <caiocesarneme@gmail.com>
2023-10-22 11:40:46 -0300

    Feat(Payments): add TicketTypeId

--- src/services/ticketApi.js
+++ src/services/ticketApi.js
@@ -1,29 +1,25 @@
 import api from './api';
 
 export async function getTicketTypes(token) {
-    try {
-        const response = await api.get('http://localhost:4000/tickets/types', {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-
-        return response.data;
-    } catch (err) {
-        console.error(err.data);
-    }
+  const response = await api.get('/tickets/types', {
+      headers: {
+          Authorization: `Bearer ${token}`,
+      },
+  });
+  // console.log(response.data);
+  return response.data;
 }
 
-export async function createTicket(body, token) {
-    try {
-        const response = await api.post('http://localhost:4000/tickets', body, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
+// export async function createTicket(body, token) {
+//     try {
+//         const response = await api.post('http://localhost:4000/tickets', body, {
+//             headers: {
+//                 Authorization: `Bearer ${token}`,
+//             },
+//         });
 
-        return response.data;
-    } catch (err) {
-        console.error(err.data);
-    }
-}
+//         return response.data;
+//     } catch (err) {
+//         console.error(err.data);
+//     }
+// }
